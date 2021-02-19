733b2c648d168cf9a3989a47e1327a79bc1397c1
Brock Brown <brock@ideapad.local>
2021-02-19 10:28:18 -0700

    path, alias added

--- install.sh
+++ install.sh
@@ -12,3 +12,5 @@ sudo apt install vim brave-browser nmap grun -y
 sudo add-apt-repository ppa:appimagelauncher-team/stable && sudo apt update -y && sudo apt install appimagelauncher -y
 
 curl -s https://api.github.com/repos/brrd/Abricotine/releases/latest | grep "browser_download_url.*deb" | grep x64 | cut -d : -f 2,3 | tr -d \" | wget -qi - && mv *.deb ~/Downloads/deb/abricotine-ub2.deb && sudo apt install ~/Downloads/deb/abricotine-ub2.deb
+echo "source $wd/.path"
+echo "source $wd/.alias"
